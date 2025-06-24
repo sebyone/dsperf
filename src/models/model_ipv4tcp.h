efe8bf3fa6da3d59a81b764dbcc9fc318befa980
Paolo Lo Giacco <plogiacco@smartlab.it>
2025-06-24 02:18:07 +0200

    last c

--- src/models/model_ipv4tcp.h
+++ src/models/model_ipv4tcp.h
@@ -67,17 +67,6 @@
 #define MAX_REMOTE_LINKS 64
 #define LINK_MAX_VAL 6
 
-// ipv4_tcp
-#define PACKET_BUFFER_MAX_SIZE 2048
-//
-#define MIN_PORT 1
-#define MAX_PORT 65535
-//
-#define MAX_LINE_LEN 256
-#define MAX_LINKS 5
-#define MAX_REMOTE_LINKS 64
-#define LINK_MAX_VAL 6
-
 enum ipv4tcp_vars // Test Model Indicators
 {
     _tstcounter = 0,
